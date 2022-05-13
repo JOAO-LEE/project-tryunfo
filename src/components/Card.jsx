7657a40576568b8f3d60a832235f99d16345d9c4
João Lee <joaumlimaum@gmail.com>
2022-05-13 18:59:39 -0300

    Requisito 3 concluído

---
+++ src/components/Card.jsx
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+
+export default class Card extends Component {
+  render() {
+    const { cardName, cardDescription,
+      cardAttr1, cardAttr2, cardAttr3,
+      cardImage, cardRare, cardTrunfo } = this.props;
+    return (
+      <>
+        <h5 data-testid="name-card">{ cardName }</h5>
+        <img src={ cardImage } alt={ cardName } data-testid="image-card" />
+        <p data-testid="description-card">{ cardDescription }</p>
+        <p data-testid="attr1-card">{ cardAttr1 }</p>
+        <p data-testid="attr2-card">{ cardAttr2 }</p>
+        <p data-testid="attr3-card">{ cardAttr3 }</p>
+        <p data-testid="rare-card">{ cardRare }</p>
+        { cardTrunfo === true ? <p data-testid="trunfo-card">Super Trunfo</p> : <> </> }
+      </>
+    );
+  }
+}
